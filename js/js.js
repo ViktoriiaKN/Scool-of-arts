92d2f6869ab2677cd0ac1d22a46b3334fc7c77c0
ViktoriiaKN <113258083+ViktoriiaKN@users.noreply.github.com>
2023-01-29 18:51:33 +0100

    Keys

--- js/js.js
+++ js/js.js
@@ -1,14 +1,31 @@
-const profile = {
-  name: "Jacques Gluke",
-    tag: "jgluke",
-    location: 'Ocho Rios, Jamaika',
-  avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/r_oy/128.jpg',
-  stats: {
-    followers: 5603,
-    views: 4827,
-    likes: 1308,
-  },
+// Деструктуризація масиву, на відміну від деструктуризації об'єктів, виконується по порядку:
+
+/* const rgb = [255, 100, 80]; */
+/* const [a, b, c] = rgb;  */// Запис: квадратны дужки зліва від знаку дорівнює, а з права масив, що деструктуризується
+
+/* console.log(a, b, c); */ // У якому порядку записали змынны - в такому порядку й записуються значення,
+// тобто а = 255, b = 100, с = 80.
+
+// Якщо потрібно пропустити якусь властивість, то це зробить оператор кома:
+/* const rgb = [255, 100, 80]; */
+/* const [a, , c] = rgb; */ // Пропускаємо властивість
+/* console.log(a, c); */
+
+const authors = {
+  kiwi: 4,
+  poly: 7,
+  ajax: 9,
+  mango: 6,
+};
+
+/* const ratings = Object.values(authors); */
+
+/* console.log(Math.max(...ratings)); */ // Спосіб отримання найвищого рейтингу
+
+const ratings = Object.keys(authors);
+
+for (const key of keys) {
+  console.log(key);
+  console.log(authors[key]);
 }
 
-const { name, tag, location, avatar, stats } = profile;
-console.log(name, tag, location, avatar, stats);
