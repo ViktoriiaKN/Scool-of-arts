329ba42d6464bdfa68eb612e3dc72e2f67a82bbe
ViktoriiaKN <113258083+ViktoriiaKN@users.noreply.github.com>
2023-02-23 18:39:35 +0100

    дльжтщтщь

--- js/js.js
+++ js/js.js
@@ -1,31 +1,7 @@
-// Деструктуризація масиву, на відміну від деструктуризації об'єктів, виконується по порядку:
+// НАВІГАЦІЯ ПО DOM
+// Властивості навігації по DOM-вузлах
 
-/* const rgb = [255, 100, 80]; */
-/* const [a, b, c] = rgb;  */// Запис: квадратны дужки зліва від знаку дорівнює, а з права масив, що деструктуризується
+const navEL = document.querySelector('.site-nav');
 
-/* console.log(a, b, c); */ // У якому порядку записали змынны - в такому порядку й записуються значення,
-// тобто а = 255, b = 100, с = 80.
-
-// Якщо потрібно пропустити якусь властивість, то це зробить оператор кома:
-/* const rgb = [255, 100, 80]; */
-/* const [a, , c] = rgb; */ // Пропускаємо властивість
-/* console.log(a, c); */
-
-const authors = {
-  kiwi: 4,
-  poly: 7,
-  ajax: 9,
-  mango: 6,
-};
-
-/* const ratings = Object.values(authors); */
-
-/* console.log(Math.max(...ratings)); */ // Спосіб отримання найвищого рейтингу
-
-const ratings = Object.keys(authors);
-
-for (const key of keys) {
-  console.log(key);
-  console.log(authors[key]);
-}
-
+const firstNavItemEl = navEL.querySelector('site-nav__item');
+console.log(firstNavItemEl);
