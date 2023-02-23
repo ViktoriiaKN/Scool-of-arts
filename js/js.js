519dcfb67d33f58d5829588555ad51746312b461
ViktoriiaKN <113258083+ViktoriiaKN@users.noreply.github.com>
2023-02-23 19:10:43 +0100

    vdbxfn

--- js/js.js
+++ js/js.js
@@ -1,7 +1,9 @@
-// НАВІГАЦІЯ ПО DOM
-// Властивості навігації по DOM-вузлах
-
-const navEL = document.querySelector('.site-nav');
-
-const firstNavItemEl = navEL.querySelector('site-nav__item');
-console.log(firstNavItemEl);
+// Вставка вузлів: appendChild(elem), insertBefore(elem, nextSibling), append(...elems), prepend(...elems);
+// Створюємо заголовок:
+const titleEl = document.createElement(''); // В дужки передаємо валідний (якщо передати не валідний тег,
+// то він його створить також, що нам не потрібно) тег, який потрібно створити
+// елемент за допомогою createElement('') створюється у пам'яті, в розмітці він не створюється
+// Додавати елемент до розмітки будемо тоді, коли його підготуємо:
+titleEl.classList.add('page-title');
+titleEl.textContent.add('Це заголовок сторінки');
+console.log(titleEl); // Ми зконструювали DOM-element в пам'яті 
